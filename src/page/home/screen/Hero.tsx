e41938a27db19e6f58be3339fe92c49cc17ae968
Believe <117173080+Beve-hub@users.noreply.github.com>
2024-04-21 09:30:06 +0000

    stack

--- src/page/home/screen/Hero.tsx
+++ src/page/home/screen/Hero.tsx
@@ -3,9 +3,22 @@ import img from '../../../assets/image/image_1.png';
 const Hero: React.FC = () => { 
 
   return (
-   <div>
+    <section id="home" className="min-h-screen  overflow-hidden">
     <img src={img} className="w-screen object-cover h-screen" alt='' /> 
-   </div>
+    <div className="absolute top-[17rem]  grid gap-4 overflow-hidden">
+      <div className='grid justify-center w-[40rem]'>
+      <div className='grid gap-2 '>
+      <h1 className=" font-bold text-[--bg-color] text-2xl  text-balance md:text-balance">Welcome To Grand Express Delivery Service</h1>
+      <p className="text-xl text-[--bg-color] text-start md:text-balance">For your Safe & Reliable shipping</p>
+      </div>
+      <div className='flex gap-1'>
+        <input type='text' placeholder='Tracking Number' className="h-full  bg-[--bg-color]  pl-2 pr-19 text-gray-500  sm:text-sm"/>
+        <button className='bg-[--button-color] py-[0.6rem] px-5'>Track</button>
+      </div>
+      </div>
+      
+    </div>
+   </section>
   )
 }
 
--- src/page/home/screen/Package.tsx
+++ src/page/home/screen/Package.tsx
@@ -1,10 +1,62 @@
-import React from 'react'
+import img from '../../../assets/image/image_5.png';
+import { SlCalender } from "react-icons/sl";
+import { BsTruck } from "react-icons/bs";
+import { GrLocation } from "react-icons/gr";
+import { AiOutlineDollarCircle } from "react-icons/ai";
 
-const Package = () => {
+
+
+const Package: React.FC = () => {
   return (
+    <section  className="min-h-[30rem] py-20 flex  md:flex-row grid-col-2 items-center justify-center ">
     <div>
-      package
+      <div className='flex gap-2 md:grid-cols-2 '>
+       
+          <div className='bg-[--bg-color] drop-shadow-xl py-4'>
+            <div className='p-6'>              
+            <p className='font-bold py-4 text-2xl text-[#121212]'>PACKING & STORAGE</p>
+                <p className='py-4 text-lg text-[#121212] font-medium '>For Business Only</p>
+                <p className='text-light text-[#121212] text-balance md:text-balance'>GEDS Bringing you industry-specific expertise; whatever you’re shipping, wherever you’re shipping it.</p>
+             
+            </div>
+              <div className='bg-[--secondary-t-color] my-2 py-2'>
+              <p className='p-2 text-lg text-[#121212] font-medium text-start sm:text-center'>Manage your shipments</p>
+              <div className='  grid  items-center md:grid-cols-2 gap-4 px-10 '>
+                <div className='flex items-center gap-4'>
+                <SlCalender color='white' size={24} className="p-1 rounded-lg bg-[--primary-t-color]"/>
+                  <p>Book a shippment</p>
+                </div>
+                <div className='flex items-center gap-4'>
+                <GrLocation color='white' size={24} className="p-1 rounded-lg bg-[--primary-t-color]"/>
+                  <p>Find Location</p>
+                </div>
+                <div className='flex items-center gap-4'>
+                <BsTruck color='white' size={24} className="p-1 rounded-lg bg-[--primary-t-color]"/>
+                  <p>Redirect your Package</p>
+                </div>
+                <div className='flex items-center gap-4'>
+                <AiOutlineDollarCircle color='white' size={24} className="p-1 rounded-lg bg-[--primary-t-color]"/>
+                  <p>Fuel Surcharge</p>
+                </div>
+              </div>              
+              </div>
+              <div className='flex justify-center'>
+                 <button className='bg-[--button-color] px-[7rem] py-2 '>
+                  Explore GEDS
+                </button>
+              </div>
+                
+           
+         </div>
+
+   
+         <div className='sm:grid hidden'>
+           <img src={img} alt="" className='w-[30rem] h-[30rem]'/>
+          </div>
+       
+      </div>
     </div>
+  </section>
   )
 }
 
