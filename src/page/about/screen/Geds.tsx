34e5e5ef77b4640dc00b27ff451ffaacb9e36be0
Believe <117173080+Beve-hub@users.noreply.github.com>
2024-04-22 02:31:54 +0000

    asfhdgf

--- src/page/about/screen/Geds.tsx
+++ src/page/about/screen/Geds.tsx
@@ -1,11 +1,97 @@
-import React from 'react'
+import img from '../../../assets/image/image_6.png';
+import { BiWorld } from "react-icons/bi";
+import { MdTimer } from "react-icons/md";
+import { RiCustomerServiceFill } from "react-icons/ri";
+import { FaUserTie } from "react-icons/fa";
 
 
 const Geds: React.FC = () => {
     return (
-        <section id="join" className="min-h-[20rem] max-w-full ">
-           <p>helloworld</p> 
-        </section>
+        <section  className="min-h-[30rem] py-20 flex md:flex-row grid-col-2 items-center justify-center ">
+
+        <div>
+        <div className='p-6'>              
+                <p className='font-bold py-4 text-2xl text-[#121212]'>GRAND EXPRESS DELIVERY SERVICE (GEDS)</p>
+                    <p className='py-4 text-lg text-[#121212] font-medium '>The undisputed global leader in international express shipping</p>
+                    <p className='text-light text-[#121212] text-balance md:text-balance'>When your shipment needs to be there fast, choose the International Specialists for quick, reliable expedited shipments to and from more than 220 countries and territories.</p>
+                 
+        </div>
+        <div>
+
+      
+          <div className='flex gap-2 md:grid-cols-2 '> 
+            
+           
+    
+              <div className='  grid  items-center md:grid-cols-2 gap-4 px-10 '>
+
+
+               <div className='bg-[--bg-color] drop-shadow-xl py-4 max-w-[15rem] '>
+                 <div className='grid items-center gap-4 p-4'>
+                  <BiWorld color='#2f0095' size={30} />
+                    <p className='font-bold text-md text-[#FD715E]'>International Expertise</p>
+                    <div>
+                        <p>With more than a half-century of experience and a team of more than 100,000 Certified International Specialists, no other provider knows the world like we do.</p>
+                    </div>
+                  </div>                           
+             
+               </div>
+
+               <div className='bg-[--bg-color] drop-shadow-xl py-4 max-w-[15rem] '>
+                 <div className='grid items-center gap-4 p-4'>
+                  <MdTimer color='#2f0095' size={30} />
+                    <p className='font-bold text-md text-[#FD715E]'>Extraordinary Speed</p>
+                    <div>
+                        <p>We leverage our unrivaled global expertise and Customs know-how to keep your shipments moving swiftly – providing door-to-door expedited shipment delivery on the next possible business day.</p>
+                    </div>
+                  </div>                           
+             
+               </div>
+
+               <div className='bg-[--bg-color] drop-shadow-xl py-4 max-w-[15rem] '>
+                 <div className='grid items-center gap-4 p-4'>
+                  <RiCustomerServiceFill color='#2f0095' size={30} />
+                    <p className='font-bold text-md text-[#FD715E]'>Service Excellence</p>
+                    <div>
+                        <p>Our “can-do” approach is built on innovative thinking to provide intelligent, sustainable, tailored solutions for your specific logistics challenges including express shipping.</p>
+                    </div>
+                  </div>                           
+             
+               </div>
+
+               <div className='bg-[--bg-color] drop-shadow-xl py-4 max-w-[15rem] '>
+                 <div className='grid items-center gap-4 p-4'>
+                  <FaUserTie color='#2f0095' size={30} />
+                    <p className='font-bold text-md text-[#FD715E]'>Trusted Advice</p>
+                    <div>
+                        <p>We are passionate about helping GEDS customers succeed by guiding you into new markets to achieve international growth and maximize your cross-border potential.</p>
+                    </div>
+                  </div>                           
+             
+               </div>
+
+                                       
+             
+                                      
+              
+
+             </div>
+              
+             <div className='sm:grid hidden'>
+               <img src={img} alt="" className='w-[30rem] h-[30rem]'/>
+              </div>
+             <div className='bg-[--secondary-t-color] my-2 py-2'>               
+           
+</div>
+
+    
+         
+           
+          </div>
+        </div>
+       
+        </div>
+      </section>
     )
 }
 
--- src/page/about/screen/Header.tsx
+++ src/page/about/screen/Header.tsx
@@ -3,9 +3,18 @@ import IMG from '../../../assets/image/image_about.png'
 
 const Header: React.FC = () => {
     return (
-        <div className='mt-[10rem] grid justify-center'>
-           <img src={IMG} alt=''/>
-        </div>
+        <section className='mt-[10rem]'>
+            <div className='grid justify-center'>
+                <img src={IMG} alt='' />
+                <div className='absolute grid justify-center '>
+                    <div>
+                    <p className='text-white font-bold'>HOME</p>
+                    <p className='text-white'>HOME | ABOUT</p>
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
     )
 }
 
